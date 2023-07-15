7423fda7ad049bcd2c7bfaf3db909471acb8308a
Ricardo Augusto Coelho <114035092+NlCoelho@users.noreply.github.com>
2023-07-15 03:25:45 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,10 @@
-function Home(){
-    return <h1>Não se aproxime de uma cabra pela frente, de um cavalo por trás ou de um idiota por qualquer dos lados.</h1>;
- 
+function Home() {
+  return (
+    <h1>
+      Não se aproxime de uma cabra pela frente, de um cavalo por trás ou de um
+      idiota por qualquer dos lados.
+    </h1>
+  );
 }
 
 export default Home;
